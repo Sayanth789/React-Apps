91dfe1d71390d95fb079eb4a9425d57849d15a7c
Sayanth <sayanthstrng@gmail.com>
2025-10-20 18:42:22 +0530

    editing  code to adjust with backend

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -10,31 +10,16 @@ function App() {
   const [cart, setCart] = useState([]);
 
 
-  // useEffect(() => {
-  //   const fetchAppData = async () => {
-  //     const response = await axios.get('/api/cart-items?expand=product');
-  //     setCart(response.data);
-  //   };
-
-  //   fetchAppData();
-  // }, []);
-
   useEffect(() => {
-    const mockCart = [
-      { 
-        product: { id: 1, name: 'Product 1', priceCents: 1000, image: 'images/product1.png' },
-        quantity: 1,
-        deliveryOptionId: 1
-      },
-      { 
-        product: { id: 2, name: 'Product 2', priceCents: 2000, image: 'images/product2.png' },
-        quantity: 2,
-        deliveryOptionId: 2
-      }
-    ];
+    const fetchAppData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product');
+      setCart(response.data);
+    };
 
-    setCart(mockCart);
+    fetchAppData();
   }, []);
+
+  
 
 
   return (

3b5f319cb20be4572ee25363c4a0a786f936ea42
Sayanth <sayanthstrng@gmail.com>
2025-10-20 18:44:46 +0530

    Fetch products from API instead of using mock data
    Replaced mock product data with API call to fetch products.

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -8,22 +8,14 @@ import './HomePage.css';
 export function HomePage({ cart }) {
     const [products, setProducts] = useState([]);
 
-    // useEffect(() => {
-    //     const getHomeData = async () => {
-    //         const response = await axios.get('/api/products');
-    //         setProducts(response.data);
-    //     };
-        
-    //     getHomeData();
-    // }, [])
     useEffect(() => {
-        const mockProducts = [
-            { id: 1, name: 'Product 1', priceCents: 1000, image: 'images/product1.png' },
-            { id: 2, name: 'Product 2', priceCents: 2000, image: 'images/product2.png' },
-            { id: 3, name: 'Product 3', priceCents: 3000, image: 'images/product3.png' },
-        ];
-        setProducts(mockProducts);
-    }, []);
+        const getHomeData = async () => {
+            const response = await axios.get('/api/products');
+            setProducts(response.data);
+        };
+        
+        getHomeData();
+    }, [])
 
 
 

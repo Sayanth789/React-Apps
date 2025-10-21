86181124af06669c17215877dbf6fd93c15cbff4
Sayanth <SayanthStrng@gmail.com>
2025-10-21 10:51:01 +0530

    Changing to use async await than using the promise

--- ecommerce-project/src/pages/orders/OrderPage.jsx
+++ ecommerce-project/src/pages/orders/OrderPage.jsx
@@ -9,10 +9,13 @@ export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/orders?expand=products')
-      .then((response) => {
-        setOrders(response.data);
-      });
+
+    const fetchOrderData = async () => {
+      const response = axios.get('/api/orders?expand=products');
+      setOrders(response.data);
+    }
+    
+    fetchOrderData();
   }, []);
 
 

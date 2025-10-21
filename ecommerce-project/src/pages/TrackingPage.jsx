c9c99d44b5118d17e4280e3ac4334e1207ad41ab
Sayanth <SayanthStrng@gmail.com>
2025-10-21 12:05:25 +0530

    Updating Router so that tracking page can use the link

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,13 +1,18 @@
 import { Header } from '../components/Header';
 import './TrackingPage.css'
+import { useParams } from 'react-router';
+
+
 
 export function TrackingPage({ cart }) {
+    const { orderId , productId } = useParams();
+    
     return (
          <>
       <title>Tracking</title>
       <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
         <Header cart={cart} />
-        
+
       <div className="tracking-page">
         <div className="order-tracking">
           <a className="back-to-orders-link link-primary" href="/orders">

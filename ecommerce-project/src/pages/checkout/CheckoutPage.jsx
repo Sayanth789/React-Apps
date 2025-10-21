2f15bb37adb0372b22239aa0c7353e3dcdf7a715
Sayanth <SayanthStrng@gmail.com>
2025-10-21 11:44:31 +0530

    Seperatin CheckoutHeader to component and display the qunatity

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { OrderSummary } from './OrderSummary';
 import { PaymentSummary } from './PaymentSummary';
+import { CheckoutHeader } from './CheckoutHeader';
 import './checkout-header.css';
 import './CheckoutPage.css';
 
@@ -30,25 +31,7 @@ export function CheckoutPage({ cart,  loadCart }) {
     return (
         <>
             <title>Checkout</title>
-            <div className='checkout-header'>
-                <div className='header-content'>
-                    <div className='checkout-header-left-section'>
-                        <a href='/'>
-                            <img className='logo' src='images/logo.png' />
-                            <img className='moblie-logo' src='images/mobile-logo.png' />
-                        </a>
-                    </div>
-                    <div className='checkout-header-middle-section' >
-                        Checkout (<a className='return-to-home-link'
-                        href='/3'>3 items</a>)
-                    </div>
-
-                    <div className='checkout-header-right-section'>
-                        <img src='images/icons/checkout-lock-icon.png' />
-                    </div>
-                </div>
-            </div>
-
+            <CheckoutHeader cart={cart} />
             <div className='checkout-page'>
                 <div className='page-title'>Review your order</div>
 

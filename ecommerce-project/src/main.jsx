291cb9e4a653bdca384e0ab802961d304c7fd6b4
Sayanth <SayanthStrng@gmail.com>
2025-10-21 11:17:20 +0530

    Adding strict mode back

--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -5,11 +5,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </StrictMode>,
+  </StrictMode>,
 
-  // Removing the strict mode temperorarly
+  // Adding back the strictMode 
 )

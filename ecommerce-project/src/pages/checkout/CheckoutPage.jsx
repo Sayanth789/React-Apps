0802be895de6eb135bb38651a16a5ec5b0289fd4
Sayanth <SayanthStrng@gmail.com>
2025-10-22 09:32:53 +0530

    Adding Tests

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -23,7 +23,7 @@ export function CheckoutPage({ cart,  loadCart }) {
         };
         
         fetchCheckoutData();
-    }, []);
+    }, [cart]);
 
    
 
--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -11,5 +11,10 @@ export default defineConfig({
       }
       
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiels: './setupTests.js',
   }
 })

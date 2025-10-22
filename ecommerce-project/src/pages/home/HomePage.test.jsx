23d2de4e637dc1569e1ed35af082a910f05de5c8
Sayanth <SayanthStrng@gmail.com>
2025-10-22 10:53:41 +0530

    Updating the Add to cart

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -56,7 +56,7 @@ describe('HomePage component', () => {
       </MemoryRouter>
     );
 
-    const productContainers = await screen.findAllByTestId('product-container');
+    const productContainers = await screen.findAllByTestId('product-container');   
 
     await waitFor(() => {
       expect(productContainers.length).toBe(2);
@@ -80,9 +80,19 @@ describe('HomePage component', () => {
 
     const productContainers = await screen.findAllByTestId('product-container');
 
+    const quantitySelector1 = within(productContainers[0])
+        .getByTestId('product-quantity-selector');
+    await user.selectOptions(quantitySelector1, '2');
+    
+
     const addToCartButton1 = within(productContainers[0]).getByTestId('add-to-cart-button');
     await user.click(addToCartButton1);
 
+    const quantitySelector2 = within(productContainers[1])
+        .getByTestId('product-quantity-selector');
+    await user.selectOptions(quantitySelector2, '3');
+    
+    
     const addToCartButton2 = within(productContainers[1]).getByTestId('add-to-cart-button');
     await user.click(addToCartButton2);
 
@@ -91,7 +101,7 @@ describe('HomePage component', () => {
       '/api/cart-items',
       {
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1,
+        quantity: 2,
       }
     );
 
@@ -100,7 +110,7 @@ describe('HomePage component', () => {
       '/api/cart-items',
       {
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1,
+        quantity: 3,
       }
     );
 
